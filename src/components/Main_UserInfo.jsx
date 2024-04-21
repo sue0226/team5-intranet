b02e99d4c77323d115b5ae8eda7308dec055c463
sue0226 <kiaide.go@gmail.com>
2024-04-21 10:51:10 +0000

    Fix: 비밀번호 변경 위치 수정

--- src/components/Main_UserInfo.jsx
+++ src/components/Main_UserInfo.jsx
@@ -2,10 +2,54 @@ import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
 import Main_UserProfileImage from './Main_UserProfileImage';
 import Main_UserInfoInput from './Main_UserInfoInput';
-import Main_UserInfoPwChange from './Main_UserInfoPwChange';
 import Button from './Button';
 
+import { collection, query, where, getDocs, updateDoc } from "firebase/firestore";
+import { db } from '../core/firebase'; // Firebase 설정 파일에서 db를 가져옵니다.
+
+
 function Main_UserInfo() {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+  useEffect(() => {
+    if (password === confirmPassword) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  }, [password, confirmPassword]);
+
+  const handleButtonClick = async () => {
+    console.log('Password:', password); // password 값 출력
+    console.log('Confirm Password:', confirmPassword); // confirmPassword 값 출력
+  
+    if (password === '' || confirmPassword === '') {
+      alert('비밀번호를 입력해주세요.');
+      return;
+    }
+  
+    if (password !== confirmPassword) {
+      alert('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+    
+    const userID = window.sessionStorage.getItem('userID'); // 세션 스토리지에서 userID 가져오기
+    const q = query(collection(db, 'User'), where('userID', '==', userID));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach(async (doc) => {
+      await updateDoc(doc.ref, {
+        PW: password,
+      });
+    });
+  
+    if (password !== '') {
+      alert('비밀번호가 변경되었습니다.'); // 비밀번호 변경 성공 alert
+    }
+    window.location.reload(); // 페이지 새로고침
+  };
+
   return (
     <UserInfo>
       <Header>회원 정보 수정</Header>
@@ -19,7 +63,12 @@ function Main_UserInfo() {
         <Main_UserInfoInput labelText="이메일" value="test@test.com" disabled={true}></Main_UserInfoInput>
         <Main_UserInfoInput labelText="이름" value="신콩이" disabled={true}></Main_UserInfoInput>
       </SectionBox2>
-      <Main_UserInfoPwChange></Main_UserInfoPwChange>
+      <SectionBox3>
+        <SectionH3>비밀번호 변경</SectionH3>
+        <Main_UserInfoInput labelText="비밀번호" placeholderText="새 비밀번호를 입력하세요." value={password} onChange={(e) => setPassword(e.target.value)}></Main_UserInfoInput>
+        <Main_UserInfoInput labelText="비밀번호 확인" placeholderText="새 비밀번호를 한번 더 입력하세요." value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></Main_UserInfoInput>
+      </SectionBox3>
+      <TempoBtn disabled={isButtonDisabled} onClick={handleButtonClick}>등록</TempoBtn>
     </UserInfo>
   )
 };
@@ -102,4 +151,25 @@ const SectionBox3 = styled.div`
   width: 92%;
   height: 190px;
   box-sizing: border-box;
+`;
+
+const SectionH3 = styled.h2`
+  display: block;
+  width: 95%;
+  margin-bottom: 20px;
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const TempoBtn = styled.div`
+  width: 100px;
+  height: 40px;
+  margin: 25px;
+  border: 1px solid #C8CCE5;
+  border-radius: 10px;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
 `;

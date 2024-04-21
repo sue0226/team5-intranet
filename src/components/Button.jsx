984d40d97c36dac45913d4a7cf5d8cdb35c286e1
sue0226 <kiaide.go@gmail.com>
2024-04-21 04:50:45 +0000

    Style: 폰트 수정 및 커서 스타일 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -7,11 +7,12 @@ const Btn = styled.button`
   border-radius: 10px;
   border: 1px #C8CCE5 solid;
 
-  font-family: 'Pretendard-Regular';
+  font-family: 'Pretendard, sans-serif';
 
   background-color: white;
   box-shadow: 2px 2px 4px 0 rgba(0, 0, 0, 0.25);
   transition-duration: 0.1s;
+  cursor: pointer;
 
   &:active {
     position: relative;
